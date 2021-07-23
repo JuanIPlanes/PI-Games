36e870dd389bcd045ded2ca51f499438481feb98
Juani Planes <juaniplanes12321@outlook.com>
2021-07-23 04:45:59 -0300

    migrating to ES2015 and add specific tsconfig for routes files

--- PI-Videogames/api/src/routes/vg_id.js
+++ PI-Videogames/api/src/routes/vg_id.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,64 +27,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var express = require("express");
-var functions_js_1 = require("../functions.js");
-var router = express.Router();
-router.get('/:id', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
-    var ID, _a, _b, _c;
-    return __generator(this, function (_d) {
-        switch (_d.label) {
-            case 0:
-                if (!req.params.id)
-                    return [2 /*return*/, next()]; //Route Verificator
-                ID = parseInt(req.params.id);
-                if (typeof ID !== 'number' || Number.isNaN(ID))
-                    return [2 /*return*/, res.send({ "msg": "ID Invalido" })];
-                _b = (_a = res).send;
-                if (!
-                //! DB OPERATIVA CON 300000 slots
-                (ID <= 1300000)) 
-                //! DB OPERATIVA CON 300000 slots
-                return [3 /*break*/, 2];
-                return [4 /*yield*/, functions_js_1.getById(ID)];
-            case 1:
-                _c = _d.sent();
-                return [3 /*break*/, 3];
-            case 2:
-                _c = functions_js_1.msg("invalid");
-                _d.label = 3;
-            case 3:
-                _b.apply(_a, [_c]);
-                return [2 /*return*/];
-        }
-    });
-}); });
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = __importStar(require("express"));
+const functions_js_1 = require("../functions.js");
+const router = express.Router();
+router.get('/:id', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.params.id)
+        return next(); //Route Verificator
+    const ID = parseInt(req.params.id);
+    if (typeof ID !== 'number' || Number.isNaN(ID))
+        return res.send({ "msg": "ID Invalido" });
+    res.send(
+    //! DB OPERATIVA CON 300000 slots
+    ID <= 1300000
+        ? yield functions_js_1.getById(ID)
+        : functions_js_1.msg("invalid"));
+}));
 module.exports = router;
