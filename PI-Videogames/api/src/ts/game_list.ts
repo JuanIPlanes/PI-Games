223c3937c6b777d43e6a9d36df0b7e5c9e0615ba
Juani Planes <juaniplanes12321@outlook.com>
2021-07-23 04:34:46 -0300

    change folder name from ts-build to ts and edit tsconfig

--- PI-Videogames/api/src/ts/game_list.ts
+++ PI-Videogames/api/src/ts/game_list.ts
@@ -1,7 +1,6 @@
 import * as express from "express"
 const router = express.Router()
 
-router.get('/', async (req, res) =>
-    res.send("glist"))
+router.get('/', async (req, res) => res.send("glist"))
 
 module.exports = router;
